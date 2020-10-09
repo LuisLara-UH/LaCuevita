01b1196f937bf7fbbc78a3f4cb6791292fb5934b
LuisLara-UH <luis.lara.uh42@gmail.com>
2020-10-09 02:11:23 +0200

    Add project files.

---
+++ LaCuevita/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LaCuevita.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

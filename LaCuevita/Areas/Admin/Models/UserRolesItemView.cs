2585f262698673f522fa0fe133475b03db8f4dbc
LuisLara-UH <luis.lara.uh42@gmail.com>
2020-10-14 17:28:31 +0200

    Add UserRolesItemView Add UserRolesViewModel

---
+++ LaCuevita/Areas/Admin/Models/UserRolesItemView.cs
@@ -0,0 +1,40 @@
+﻿using LaCuevita.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static LaCuevita.Extensions.Roles.RoleExtensions;
+
+namespace LaCuevita.Areas.Admin.Models
+{
+    public class UserRolesItemView
+    {
+        private UserManager<ApplicationUser> _userManager;
+        public UserRolesItemView(ApplicationUser user, UserManager<ApplicationUser> userManager)
+        {
+            _user = user;
+
+            _userManager = userManager;
+        }
+
+        public async Task Prepare()
+        {
+            IsAdmin = await _userManager.IsInRoleAsync(_user, RoleHelper.RoleValueMap[RoleType.Admin]);
+            IsSeller = await _userManager.IsInRoleAsync(_user, RoleHelper.RoleValueMap[RoleType.Seller]);
+            IsClient = await _userManager.IsInRoleAsync(_user, RoleHelper.RoleValueMap[RoleType.Client]);
+        }
+
+        #region Properties
+        private ApplicationUser _user;
+        public ApplicationUser User
+        {
+            get => _user;
+        }
+
+        public bool IsAdmin { get; set; }
+        public bool IsSeller { get; set; }
+        public bool IsClient { get; set; }
+        #endregion
+    }
+}
---
+++ LaCuevita/Areas/Admin/Models/UserRolesViewModel.cs
@@ -0,0 +1,46 @@
+﻿using LaCuevita.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaCuevita.Areas.Admin.Models
+{
+    public class UserRolesViewModel
+    {
+        private UserManager<ApplicationUser> _userManager;
+        public UserRolesViewModel(List<ApplicationUser> users, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+
+            _users = users;
+
+            _userRoles = new List<UserRolesItemView>();
+        }
+
+        public async Task Prepare()
+        {
+            foreach (var user in _users)
+            {
+                var userRole = new UserRolesItemView(user, _userManager);
+                await userRole.Prepare();
+                _userRoles.Add(userRole);
+            }
+        }
+
+        #region Properties
+        private List<ApplicationUser> _users;
+        public List<ApplicationUser> Users
+        {
+            get => _users;
+        }
+
+        private List<UserRolesItemView> _userRoles;
+        public List<UserRolesItemView> UserRoles
+        {
+            get => _userRoles;
+        }
+        #endregion
+    }
+}

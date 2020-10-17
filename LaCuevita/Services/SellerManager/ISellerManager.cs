1d7ca787e0e24467a985be09dbde9a9e181ee024
LuisLara-UH <luis.lara.uh42@gmail.com>
2020-10-17 02:09:14 +0200

    Add ClientManager and SellerManager

---
+++ LaCuevita/Services/SellerManager/ISellerManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaCuevita.Services.SellerManager
+{
+    public interface ISellerManager
+    {
+    }
+}
